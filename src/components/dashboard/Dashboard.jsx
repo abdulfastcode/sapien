ed4e620d5c0268292cba1f140e3a14747f67fad3
abdulfastcode <abdul@fastcode.ai>
2024-01-19 10:20:46 +0530

    deployment error fix

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import DashBoardHeader from "./DashBoardHeader";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import LeftNavBar from "./LeftNavBAr";
+import LeftNavBar from "./LeftNavBar";
 
 const Dashbord = () => {
   const navigate = useNavigate();
